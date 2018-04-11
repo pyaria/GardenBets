18c85557cbf4d589405fac8e2c11982f582f9747
Pie <jadetong@gmail.com>
2018-04-10 21:18:49 -0700

    registered plants app

--- GardenBets/settings.py
+++ GardenBets/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'games.apps.GamesConfig'
+    'games.apps.GamesConfig',
+    'plants.apps.PlantsConfig',
 ]
 
 MIDDLEWARE = [
