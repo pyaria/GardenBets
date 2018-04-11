5a22ad7879a06afb87172275161507f8b749355f
Pie <jadetong@gmail.com>
2018-04-10 23:00:05 -0700

    added board model

--- games/models.py
+++ games/models.py
@@ -1,7 +1,90 @@
 from django.db import models
+from plants.models import Seed
 
 # Create your models here.
-# class Board(models.Model):
-#     slot_1a = models.Ch
+class Board(models.Model):
+    slot_1a = models.CharField(max_length=100, null=True)
+    slot_1b = models.CharField(max_length=100, null=True)
+    slot_1c = models.CharField(max_length=100, null=True)
+    slot_1d = models.CharField(max_length=100, null=True)
+    slot_1e = models.CharField(max_length=100, null=True)
+    slot_1f = models.CharField(max_length=100, null=True)
+
+    slot_2a = models.CharField(max_length=100, null=True)
+    slot_2b = models.CharField(max_length=100, null=True)
+    slot_2c = models.CharField(max_length=100, null=True)
+    slot_2d = models.CharField(max_length=100, null=True)
+    slot_2e = models.CharField(max_length=100, null=True)
+    slot_2f = models.CharField(max_length=100, null=True)
+
+    slot_3a = models.CharField(max_length=100, null=True)
+    slot_3b = models.CharField(max_length=100, null=True)
+    slot_3c = models.CharField(max_length=100, null=True)
+    slot_3d = models.CharField(max_length=100, null=True)
+    slot_3e = models.CharField(max_length=100, null=True)
+    slot_3f = models.CharField(max_length=100, null=True)
+
+    slot_4a = models.CharField(max_length=100, null=True)
+    slot_4b = models.CharField(max_length=100, null=True)
+    slot_4c = models.CharField(max_length=100, null=True)
+    slot_4d = models.CharField(max_length=100, null=True)
+    slot_4e = models.CharField(max_length=100, null=True)
+    slot_4f = models.CharField(max_length=100, null=True)
+
+    slot_5a = models.CharField(max_length=100, null=True)
+    slot_5b = models.CharField(max_length=100, null=True)
+    slot_5c = models.CharField(max_length=100, null=True)
+    slot_5d = models.CharField(max_length=100, null=True)
+    slot_5e = models.CharField(max_length=100, null=True)
+    slot_5f = models.CharField(max_length=100, null=True)
+
+    slot_6a = models.CharField(max_length=100, null=True)
+    slot_6b = models.CharField(max_length=100, null=True)
+    slot_6c = models.CharField(max_length=100, null=True)
+    slot_6d = models.CharField(max_length=100, null=True)
+    slot_6e = models.CharField(max_length=100, null=True)
+    slot_6f = models.CharField(max_length=100, null=True)
+
+    slot_7a = models.CharField(max_length=100, null=True)
+    slot_7b = models.CharField(max_length=100, null=True)
+    slot_7c = models.CharField(max_length=100, null=True)
+    slot_7d = models.CharField(max_length=100, null=True)
+    slot_7e = models.CharField(max_length=100, null=True)
+    slot_7f = models.CharField(max_length=100, null=True)
+
+    slot_8a = models.CharField(max_length=100, null=True)
+    slot_8b = models.CharField(max_length=100, null=True)
+    slot_8c = models.CharField(max_length=100, null=True)
+    slot_8d = models.CharField(max_length=100, null=True)
+    slot_8e = models.CharField(max_length=100, null=True)
+    slot_8f = models.CharField(max_length=100, null=True)
+
+    slot_9a = models.CharField(max_length=100, null=True)
+    slot_9b = models.CharField(max_length=100, null=True)
+    slot_9c = models.CharField(max_length=100, null=True)
+    slot_9d = models.CharField(max_length=100, null=True)
+    slot_9e = models.CharField(max_length=100, null=True)
+    slot_9f = models.CharField(max_length=100, null=True)
+
+    slot_10a = models.CharField(max_length=100, null=True)
+    slot_10b = models.CharField(max_length=100, null=True)
+    slot_10c = models.CharField(max_length=100, null=True)
+    slot_10d = models.CharField(max_length=100, null=True)
+    slot_10e = models.CharField(max_length=100, null=True)
+    slot_10f = models.CharField(max_length=100, null=True)
+
+    slot_11a = models.CharField(max_length=100, null=True)
+    slot_11b = models.CharField(max_length=100, null=True)
+    slot_11c = models.CharField(max_length=100, null=True)
+    slot_11d = models.CharField(max_length=100, null=True)
+    slot_11e = models.CharField(max_length=100, null=True)
+    slot_11f = models.CharField(max_length=100, null=True)
+
+    slot_12a = models.CharField(max_length=100, null=True)
+    slot_12b = models.CharField(max_length=100, null=True)
+    slot_12c = models.CharField(max_length=100, null=True)
+    slot_12d = models.CharField(max_length=100, null=True)
+    slot_12e = models.CharField(max_length=100, null=True)
+    slot_12f = models.CharField(max_length=100, null=True)
 
 # class Round(models.Model):
